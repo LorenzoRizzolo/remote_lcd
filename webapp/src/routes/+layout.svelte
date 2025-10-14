600904e3a5f76db3b4f076918dea8a26a1b65e0f
lorenzorizzolo <lolloriz36@gmail.com>
2025-10-14 11:11:47 +0200

    add manifest.json for webapp installation, button css pointer

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
 	import "../app.css"
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<title>RemoteLCD</title>
 </svelte:head>
 
 <header>
